c8a49c237f2a0b55dc1bf9312572244339be5545
Jaspreet <jaspreet.sethi@barco.com>
2024-07-02 17:06:58 +0530

    add python data analytic

---
+++ python_data_analytics/numpy_1_attr-types_creation-func.py
@@ -0,0 +1,199 @@
+################numpy###########
+'''
+Knowledge of numpy is neccessary and kind of prerequisite in order to learn other scientific packages, specially pandas library.
+
+In 1995, Jim Hugunin, came up with package 'Numeric' which was sucessfully followed by an alternative package known as 'Numarray'. This ambiguity led to idea of unifying these 2 packages & then Travis Oliphant came up with 'Numpy' library.
+
+Currently Numpy is open sourced and licensed under BSD
+'''
+
+####Numpy installation
+'''
+Ubuntu: sudo apt-get install python-numpy
+Fedora: sudo yum install numpy scipy
+Windows(Ananconda): conda install numpy
+'''
+
+import numpy as np
+
+####Ndarray: heart of numpy Library
+'''
+Numpy array size are fixed i.e. once we define at the time of creation, we cannot change it.
+
+Ndarray:
+Each numpy array is of type/object 'Ndarray' i.e. Ndarray is a multidimensional homogeneous(coz virtually all items within it are of same type and size) array with a predetermined number of items.
+
+dtype:
+data type of numpy array
+
+shape:
+dimensions and items in an array. A tuple of N positive integers that specifies the size for each dimension.
+E.g.(4,3) where 4 is dimension(length of outermost array i.e. total 4 arrays are defined) and 3 is length of each array
+
+axes & rank:
+dimensions are defined as axes and number of axes as rank.
+'ndim' attribute is used to get axis
+
+size:
+array size i.e. total number of elements in ndarray.
+
+itemsize:
+size in bytes of each item in array.
+
+data:
+buffer containing the actual elements of the array.
+
+creating an array(array() function):
+easiest function to define a new array.
+E.g. np.array([[1,2,3],[3,4,5]])
+Also, sequences of tuples and lists interconnected make no difference.
+E.g. np.array([[1,2,3],[3,4,5]]) and np.array([(1,2,3),(3,4,5)])
+
+'''
+
+a = np.array([1,2,3]) #single dimensional
+
+b = np.array([[1,2,3], #multi dimensional - 2 dimensional
+             [4,5,6],
+             [7,8,9]])
+
+c = np.array([[1.1,2.1,3.1], #multi dimensional - 2 dimensional
+             [4.2,5.1,6.3],
+             [7.1,8.4,9.8],
+             [10.1,11.2,12.3]])
+
+d = np.array([[ [1,2], [3,4]],    #multi dimensional - 3 dimensional
+              [ [5,6], [7,8]] ])
+
+print(type(a)) #<class 'numpy.ndarray'>
+print(type(b)) #<class 'numpy.ndarray'>
+
+
+print(a.dtype) ##int64
+print(b.dtype) ##int64
+print(c.dtype) ##float64
+
+
+print(a.shape) #(3,) or (3,1) -both are samething ##length of array is 3
+print(b.shape) #(3,3) 
+print(c.shape) #(4,3) ##length of array is 4 and each array size is 3
+print(d.shape) #(2,2,2)
+
+print(a.ndim) #1 so rank is 1
+print(b.ndim) #2 so rank is 2
+print(c.ndim) #2 so rank is 2
+print(d.ndim) #3 so rank is 3
+
+print(a.size) #3
+print(b.size) #9
+print(c.size) #12
+print(d.size) #8
+
+
+print(a.itemsize) #8 i.e. 8 bytes
+print(b.itemsize) #8 
+print(c.itemsize) #8
+print(d.itemsize) #8
+
+print(a.data) #<memory at 0x7f4636fd1c80>
+print(b.data) #<memory at 0x7f463aa89210>
+print(c.data) #<memory at 0x7f463aa89210>
+print(d.data) #<memory at 0x7f4636f7e450>
+
+
+
+######dtype Option:
+'''
+We can explicitly define the dtype of array() by using 'dtype' option.
+'''
+
+f = np.array([[1,2,3], [4,5,6]], dtype = complex)
+print(f)
+'''
+output:
+[[1.+0.j 2.+0.j 3.+0.j]
+ [4.+0.j 5.+0.j 6.+0.j]]
+'''
+
+
+######Generating arrays with initial contents:
+'''
+zeros():
+will create full numpy array of zeros with dimensions defined by shape argument.
+default dtype is float64. E.g. np.zeroes((3,3))
+
+ones():
+will create full numpy array of one's with dimensions defined by shape argument.
+default dtype is float64. E.g. np.ones((3,3))
+
+arange():
+generate numpy arrays with numerical sequences based on the passed arguments.
+Similar to range function.
+syntax: np.arange(start,end,interval) -- will generate numpy array from start to end-1 with desired interval(interval can also be float)
+E.g. np.arange(0,10)
+
+reshape():
+Divides a linear array in to different parts based on arguments passed.
+syntax: reshape(rows, columns)
+E.g. a.reshape(3,4) - with divide array a in to 3 rows and 4 columns
+We can use this with arange() function to generate multidimensional arrays.
+Will give error if array size is smaller then the dimensions specified. E.g. a = np.array[1,2,3], a.reshape(3,3)
+
+linspace():
+syntax: linspace(start,end,num_of_elements)
+similar to arange but instead of interval, 3rd argument defines the number of elements in which we want the interval to split.
+E.g. np.linspace(0,10,5) -- [ 0.   2.5  5.   7.5 10. ]
+
+random():
+random() function of numpy.random module.
+This will generate random array with number of elements as specified in argument.
+E.g. np.random.random(3)  -- [0.45525291 0.90483562 0.57916089]
+np.random.random((3,3))
+'''
+
+zeroes = np.zeros((3,3))
+print(zeroes)
+''''
+[[0. 0. 0.]
+ [0. 0. 0.]
+ [0. 0. 0.]]
+'''
+
+ones = np.ones((3,3))
+print(ones)
+'''
+[[1. 1. 1.]
+ [1. 1. 1.]
+ [1. 1. 1.]]
+'''
+
+a = np.arange(0,9)
+print(a) ##[0 1 2 3 4 5 6 7 8]
+
+a = np.arange(0,9,3)
+print(a) ##[0 3 6]
+
+a = np.arange(0,5,.6)
+print(a) #[0.  0.6 1.2 1.8 2.4 3.  3.6 4.2 4.8]
+
+a = np.arange(0,9).reshape(3,3)
+print(a)
+'''
+[[0 1 2]
+ [3 4 5]
+ [6 7 8]]
+'''
+
+a = np.linspace(0,10,5)
+print(a) #[ 0.   2.5  5.   7.5 10. ]
+
+a = np.random.random(3)
+print(a) #[0.45525291 0.90483562 0.57916089]
+
+a = np.random.random((3,3))
+print(a)
+'''
+[[0.9090569  0.77058137 0.34565341]
+ [0.7969043  0.80062625 0.95076565]
+ [0.29447796 0.80668382 0.66517464]]
+'''
---
+++ python_data_analytics/numpy_2_operations_accessing.py
@@ -0,0 +1,189 @@
+import numpy as np
+#####Arithmetic operations
+'''
+addition(+):
+
+subtraction(-):
+
+multiplycation(*):
+
+divide(/):
+
+how operations happens:
+1. Operation on single array with some value/function:
+a operator 4:
+E.g. a + 4  - will add 4 to each element
+
+2. Operation on multiple arrays:
+In numpy, these operations are element-wise i.e. operations are applied only between corresponding elements, namely that occupy same position.
+a aperator b . E.g. a + b - will add each corresponding element of a to b. size of a & b should be same
+E.g. a * np.sqrt(b)
+'''
+
+a = np.arange(4)
+print(a) #[0 1 2 3]
+print(a+4) #[4 5 6 7]
+
+a = np.arange(1,4) #[1 2 3]
+b = np.arange(4,7) #[4 5 6]
+print(a+b) #[5 7 9]
+print(a-b) #[-3 -3 -3]
+print(a*b) #[4 10 13]
+
+print(a * np.sin(b)) #[-0.7568025  -1.91784855 -0.83824649]
+print(a * np.sqrt(b)) #[2.         4.47213595 7.34846923]
+
+
+A = np.arange(0,9).reshape(3,3)
+'''
+[[0 1 2]
+ [3 4 5]
+ [6 7 8]]
+'''
+
+B = np.ones((3,3))
+'''
+[[1. 1. 1.]
+ [1. 1. 1.]
+ [1. 1. 1.]]
+'''
+print(A + B)
+'''
+[[1. 2. 3.]
+ [4. 5. 6.]
+ [7. 8. 9.]]
+'''
+
+
+#############matrix product
+'''
+dot() function is use to calculate matrix product.
+The result at each position, is the sum of products between each element of the corresponding row of the first matrix with corresponding element of the corresponding column of the second matrix.
+
+syntax: np.dot(A,B) or A.dot(B)
+
+Example: np.dot(A,B)
+[[ 3.  3.  3.]
+ [12. 12. 12.]
+ [21. 21. 21.]]
+
+calculation:
+    A            B
+[[0 1 2]     [[1. 1. 1.],
+ [3 4 5]      [1. 1. 1.],
+ [6 7 8]]     [1. 1. 1.]]
+
+1st row of A
+0 1 2 
+      1  1st column of B
+      1
+      1
+0*1 + 1*1 + 2*1 = 3
+
+2nd row of A
+3 4 5 
+      1 2nd column of B
+      1
+      1
+3*1 + 4*1 + 5*1 = 12
+
+3rd row of A
+6 7 8
+      1 3rd column of B
+      1
+      1
+6*1 + 7*1 + 8*1 = 21
+
+
+Also, do note that np.dot(A,B) anf np.dot(B,A) will have different results
+'''
+
+
+######increment & decrement operators
+'''
++= -- For increasing
+-= -- For decreasing
+*= -- Fo multiplying
+
+This applies on each element of the numpy array
+'''
+
+a = np.arange(4) # [ 0 1 2 3 ]
+a += 1  # [ 1 2 3 4 ]
+
+
+######Universal Functions(ufunc)
+'''
+ufunc is a function that acts individually on each single element of the input array to generate a corresponding result in new output array.
+
+some ufunc are sqrt(), log(), sin(), etc
+
+E.g. np.sqrt(a)
+'''
+
+a = np.arange(4) #[1 2 3 4]
+print(np.sqrt(a)) #[0.         1.         1.41421356 1.73205081]
+
+
+#######Aggregate Functions
+'''
+Aggregate functions are those functions that perform an operation on a set of values, an array for example and produce a ingle result.
+
+sum(), min(), max(), mean(), std()
+'''
+a = np.arange(1,5) #[1 2 3 4]
+print(a.sum()) #10
+print(a.mean()) #2.5
+
+
+######Indexing, Slicing and Iterating
+'''
+###Accessing:
+a = np.range(1,5) #[1 2 3 4]
+a[0] -- 1
+a[3] -- 4
+a[-1] -- 4
+a[[1,3]] -- [2 4]
+
+A = np.arange(10,19).reshape((3,3))
+print(A)
+[[10 11 12]
+ [13 14 15]
+ [16 17 18]]
+
+A[1,2] -- 15 -- 1+1(2nd) row of 2+1(3rd) column as index starts from 0
+
+
+###Slicing:
+extracting portion of an array.
+Arrays obtained by slicing are views on same underlying buffer i.e. modifying sliced array will actually modify the orginial array too.
+syntax: a[start:end:n] -- start to end-1 by skipping each n-1 element
+
+a = np.arange(10) #[0 1 2 3 4 5 6 7 8 9]
+b = a[0:5]
+b[0] = 8
+print(a) #[8 1 2 3 4 5 6 7 8 9]
+print(b) #[8 1 2 3 4]
+
+a = np.arange(10) #[0 1 2 3 4 5 6 7 8 9]
+print(a[1:8:2]) #[1 3 5 7]
+print(a[::2]) #[0 2 4 6 8] -- every 2nd element
+print(a[:5:]) #[0 1 2 3 4]
+
+A = np.arange(10,19).reshape((3,3))
+print(A)
+[[10 11 12]
+ [13 14 15]
+ [16 17 18]]
+
+print(A[0,:]) #[10 11 12]
+print(A[:,0] #[10 13 16]
+print(A[0:2,0:2])
+[[10 11],
+ [13 14]]
+
+print(A[[0,2],0:2]) ## 0 & 1 column of 0th and 2nd row
+[[10 11]
+ [16 17]]
+
+'''
